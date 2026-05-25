e3ac92fa8b8511a58a0b1b08d6dd10adf5c17a04
Mauricio Urdaneta <mauriurdaneta20@gmail.com>
2026-05-25 13:57:53 -0500

    chore: creados los endpoints

--- proyect-yml/src/main/java/com/mycompany/proyect_yml/rest/HelloWorldController.java
+++ proyect-yml/src/main/java/com/mycompany/proyect_yml/rest/HelloWorldController.java
@@ -1,5 +1,6 @@
-package web.rest;
+package com.mycompany.proyect_yml.rest;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,15 +9,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorldController {
 
     @RequestMapping("/Hello")
-    public String hello(){
+    public String hello() {
         String name = "Hello";
-        name = name.replace("e","x");
+        name = name.replace("e", "x");
         return name;
     }
-    @RequestMapping("greet")
-    public String greet(){
-        return "Greetting for Spring boot!";
-    }
 
+    @GetMapping("/greet")
+    public String greet() {
+        return "Greetting for Spring boot!";
+
+}
 
 }
---
+++ proyect-yml/src/main/java/com/mycompany/proyect_yml/rest/TipoDocumentoResource.java
@@ -0,0 +1,46 @@
+package com.mycompany.proyect_yml.rest;
+
+
+import com.mycompany.proyect_yml.domain.TipoDocumento;
+import com.mycompany.proyect_yml.repository.TipoDocumentoRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+import java.util.List;
+import java.util.Set;
+
+@RestController
+@RequestMapping("/api")
+public class TipoDocumentoResource {
+
+    private final TipoDocumentoRepository tipoDocumentoRepository;
+
+    public TipoDocumentoResource(TipoDocumentoRepository tipoDocumentoRepository) {
+        this.tipoDocumentoRepository = tipoDocumentoRepository;
+    }
+
+    @PostMapping("tipo-documento")
+    public String createTipoDocument(@RequestBody TipoDocumento documentType){
+        System.out.println("Se crea: "+documentType.toString());
+        return "CREATE DOCUMENT TYPE";
+    }
+    @PutMapping("/tipo-documento")
+    public String updateTIpoDocumento(@RequestBody TipoDocumento documentType){
+        System.out.println("Se actualiza: "+documentType.toString());
+        return "UPDATE DOCUMENT TYPE";
+    }@GetMapping("/document-types")
+    public ResponseEntity<Set<TipoDocumento>> getDocumentTypes(){
+        List <TipoDocumento> tipoDocumentos =tipoDocumentoRepository.findAll() ;
+        ResponseEntity body = ResponseEntity.ok().body(tipoDocumentos);
+        return body;
+    }@GetMapping("document-type/{id}")
+    public String getTIpoDocumentoByID(@PathVariable("id") Long id){
+        return "Document type By id "+ id;
+
+    }@DeleteMapping("tipo-documento/{id}")
+    public String deleteTipoDocumento(@PathVariable("id") Long id){
+
+        return "Delete document type by id "+id;
+    }
+}
